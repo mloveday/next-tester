34cc8f9360b5dfb4f242d111e79579ffdc6b7aff
Miles Loveday <miles.loveday@flagssoftware.com>
2021-02-07 12:28:32 +0000

    Better button styling, add palette, anchor focus, visited & active state updates

--- components/furniture/Nav.tsx
+++ components/furniture/Nav.tsx
@@ -1,7 +1,7 @@
 export const Nav: React.FC = () => <nav className='flex align-center justify-center w-full max-w-6xl mx-auto'>
     <ul className='grid gap-4 grid-cols-8 w-full'>
         {['Foo', 'Bar', 'Baz'].map(content =>
-          <li key={content} className='flex align-center justify-center p-2 border'>{content}</li>
+          <li key={content} className='btn-info'>{content}</li>
         )}
     </ul>
 </nav>;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
+const colors = require('tailwindcss/colors');
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+
+      blue: colors.blue,
+      green: colors.green,
+      indigo: colors.indigo,
+      purple: colors.purple,
+      red: colors.rose,
+      teal: colors.teal,
+      yellow: colors.amber,
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ['visited'],
+    },
   },
   plugins: [],
 }
